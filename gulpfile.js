099c87e05b5ff8679783a4dd11b35becd88fc112
jabher <crbrus@gmail.com>
2014-04-22 13:02:02 +0400

    auto-generating gulp metatasks

--- gulpfile.js
+++ gulpfile.js
@@ -37,13 +37,6 @@ module.exports = function (gulp) {
         gulp.watch(['src/**/*', 'src/*'], ['build']);
     });
 
-    gulp.task('build', ['build-styles', 'build-templates', 'build-scripts', 'build-images', 'build-assets']);
-
-    gulp.task('build-styles', ['build-styles-stylus', 'build-styles-css']);
-    gulp.task('build-templates', ['build-templates-jade', 'build-templates-html']);
-    gulp.task('build-scripts', ['build-scripts-traceur']);
-    gulp.task('build-images', ['build-images-common']);
-    gulp.task('build-assets', ['build-assets-all']);
     if (ENV.isProduction) { //strict mode && minification
 
 
@@ -156,4 +149,34 @@ module.exports = function (gulp) {
             console.log("----------ERROR MESSAGE END----------\n".bold.red.underline);
         });
     }
+
+    function eachRecursive(obj, cb, path) {
+        path = path || [];
+        try {
+            Object.getOwnPropertyNames(obj).forEach(function (key) {
+                try {
+                    cb(path.concat(key), obj[key]);
+                } catch (e) {
+                }
+                eachRecursive(obj[key], cb, path.concat(key));
+            });
+        } catch (e) {
+        }
+    }
+
+    var tree = {};
+    Object.getOwnPropertyNames(gulp.tasks).forEach(function (task) {
+        var taskPath = task.split('-'), taskPathPath, treePart = tree;
+        while (taskPathPart = taskPath.shift()) {
+            treePart = treePart[taskPathPart] = treePart[taskPathPart] || {};
+        }
+
+    });
+    eachRecursive(tree, function (path, object) {
+        if (!gulp.tasks[path.join('-')]) {
+            gulp.task(path.join('-'), Object.getOwnPropertyNames(object).map(function (key) {
+                return path.concat(key).join('-')
+            }))
+        }
+    });
 };
